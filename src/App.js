bbd49a65b4d75ff4f90add0cf8c98f292ac0d25d
perrylzx <46482518+perrylzx@users.noreply.github.com>
2021-02-10 20:22:55 +0800

    Create the prototype. -Issues highlighted in comments. will resolve in the future

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
+
+import monke from './monke.gif';
 
 function App() {
+  const [monkeyPosition, setMonkeyPosition] = useState({ left: 0, top: 0 });
+  const [isMonkeyClicked, setIsMonkeyClicked] = useState(false);
+
+  useEffect(function randomizeMonkeyPosition() {
+    setMonkeyPosition({
+      // abitrary number. I want to randomize it within the range of the viewport of the screen.
+      left: Math.random() * 1000,
+      top: Math.random() * 1000,
+    });
+  }, []);
+
+  // MonkeyDance will become visible upon finding (clicking) the invisible monkey.
+  // Possible to make the hidden monkey and dancing monkey one component.
+  const MonkeyDance = styled.img.attrs({
+    src: monke,
+    alt: 'Monkey Flip',
+  })`
+    visibility: ${(props) => (props.clicked ? 'visible' : 'hidden')};
+  `;
+
+  const HiddenMonkey = styled(Button)`
+    color: blue;
+    position: fixed;
+    top: ${monkeyPosition.top}px;
+    left: ${monkeyPosition.left}px;
+    width: 100px;
+    height: 100px;
+  `;
+
+  const clickMonkey = async () => {
+    await alert('You win!');
+    setIsMonkeyClicked(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* not necessary to pass down the monkeyPosition. Directly passing it to the styled component is sufficient. */}
+      {/* instead of using css to make it invisible just render this conditionally */}
+      <MonkeyDance clicked={isMonkeyClicked} position={monkeyPosition} />
+      <HiddenMonkey onClick={() => clickMonkey()} />
     </div>
   );
 }
